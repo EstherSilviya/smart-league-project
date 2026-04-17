880f7215f814475e198a4b1666b541b1419530c3
kowsalyatk21 <kowsalytk21@gmail.com>
2026-04-17 13:44:18 +0530

    Added Admin Dashboard UI

--- smart-league-frontend/src/App.js
+++ smart-league-frontend/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Admin_Dashboard from "./pages/Admin_Dashboard/Admin_Dashboard";
+import Published from "./pages/Published/Published";
+import Categories from "./pages/Categories/Categories";
+import Drafts from "./pages/Drafts/Drafts";
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Admin_Dashboard />} />
+        <Route path="/drafts" element={<Drafts />} />
+        <Route path="/published" element={<Published />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </BrowserRouter>
   );
